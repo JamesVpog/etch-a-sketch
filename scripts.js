c45dba250b2b3905150f269aa075e814c76adb69
James Vongphasouk <james.vongpha@gmail.com>
2025-10-22 14:28:09 -0600

    feat: 16x16 grid of square divs

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
-console.log("Hello, world!")
+const container = document.querySelector("#container");
+
+for (let i = 0; i < 16 * 16; i++) {
+	const div = document.createElement("div");
+	div.style.border = "1px solid black";
+	container.appendChild(div);
+}
