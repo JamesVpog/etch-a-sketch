a3d3d7711ca1abdc2473177208a65fcbb712336f
James Vongphasouk <james.vongpha@gmail.com>
2025-10-22 15:52:01 -0600

    feat: flexible number of squares

--- scripts.js
+++ scripts.js
@@ -4,9 +4,32 @@ const container = document.querySelector("#container");
 function changeColor(e) {
 	e.target.style.backgroundColor = "red";
 }
-for (let i = 0; i < 16 * 16; i++) {
-	const div = document.createElement("div");
-	div.style.border = "1px solid black";
-	div.addEventListener("mouseover", changeColor);
-	container.appendChild(div);
+
+function clearSquares(size) {
+	// remove all previous squares 
+	while (container.firstChild) {
+		container.removeChild(container.firstChild);
+	}
+
+	const totalItems = size * size;
+	const itemSize = `calc(100% / ${size})`;
+
+
+	for (let i = 0; i < totalItems; i++) {
+		const div = document.createElement("div");
+		div.style.border = "1px solid black";
+		div.style.flex = `0 0 ${itemSize}`;
+		div.style.height = itemSize;
+		div.addEventListener("mouseover", changeColor);
+		container.appendChild(div);
+	}
 }
+
+clearSquares(16);
+
+const squareNumberButton = document.querySelector("#numSquare")
+
+squareNumberButton.addEventListener("click", () => {
+	let userGridSize = prompt("Enter a number from 1-100.");
+	clearSquares(userGridSize);
+})
