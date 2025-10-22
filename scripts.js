151730879edd74500e45b71eda48c2ecdc4ceac5
James Vongphasouk <james.vongpha@gmail.com>
2025-10-22 14:38:35 -0600

    feat: etching pen trail

--- scripts.js
+++ scripts.js
@@ -1,7 +1,12 @@
 const container = document.querySelector("#container");
 
+
+function changeColor(e) {
+	e.target.style.backgroundColor = "red";
+}
 for (let i = 0; i < 16 * 16; i++) {
 	const div = document.createElement("div");
 	div.style.border = "1px solid black";
+	div.addEventListener("mouseover", changeColor);
 	container.appendChild(div);
 }
